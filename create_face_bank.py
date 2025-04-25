83599ed80b3f8946ee6dd5e1d7f984d502f2926a
Mahdi Ahmadi Kamal <909stars@gmail.com>
2025-04-25 18:36:20 +0330

    code modified

--- create_face_bank.py
+++ create_face_bank.py
@@ -10,3 +10,18 @@ app.prepare(ctx_id=0, det_size=(640, 640))
 
 face_bank_path = "./face_bank/"
 
+face_bank = []
+for person_name in os.listdir(face_bank_path):
+    folder_path = os.path.join(face_bank_path, person_name)
+    if os.path.isdir(folder_path):
+        # print(folder_path)
+        for file_name in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, file_name)
+            # print(file_path)
+            image = cv2.imread(file_path)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            result = app.get(image)
+
+            embedding = result[0]["embedding"]
+            dict = {"name": person_name, "embedding": embedding}
+            face_bank.append(dict)
