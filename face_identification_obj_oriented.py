6007def97ba7657eec9274453af8d984be5d1732
Mahdi Ahmadi Kamal <909stars@gmail.com>
2025-04-26 22:31:20 +0330

    code modified

--- face_identification_obj_oriented.py
+++ face_identification_obj_oriented.py
@@ -11,6 +11,7 @@ class FaceIdentification:
         self.threshold = 25
 
     def load_image(self, opt):
+        print("load")
         self.input_image = cv2.imread(opt.image)
         self.results = self.app.get(self.input_image)
 
@@ -18,13 +19,39 @@ class FaceIdentification:
         self.face_bank = np.load("face_bank.npy", allow_pickle=True)
         
     def identification(self):
-        ...
+        for result in self.results:
+            cv2.rectangle(self.input_image, (int(result.bbox[0]), int(result.bbox[1])), 
+                        (int(result.bbox[2]), int(result.bbox[3])), (0, 255, 0), 2)
+        
+            for person in self.face_bank:
+                face_bank_person_embedding = person["embedding"]
+                new_person_embedding = result["embedding"]
+                distance = np.sqrt(np.sum((face_bank_person_embedding - new_person_embedding)**2))
+                if distance <= opt.threshold:
+                    cv2.putText(self.input_image, person["name"], (int(result.bbox[0])-50, int(result.bbox[1])-10),
+                                fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 0, 0), thickness=4, 
+                                lineType=cv2.LINE_AA)
+                    cv2.putText(self.input_image, person["name"], (int(result.bbox[0])-50, int(result.bbox[1])-10),
+                                fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 255, 0), thickness=2, 
+                                lineType=cv2.LINE_AA)
+                    break
+            else:
+                cv2.putText(self.input_image, "Unknown", (int(result.bbox[0])-50, int(result.bbox[1])-10),
+                                fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 0, 0), thickness=4, 
+                                lineType=cv2.LINE_AA)
+                cv2.putText(self.input_image, "Unknown", (int(result.bbox[0])-50, int(result.bbox[1])-10),
+                                fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 0, 255), thickness=2, 
+                                lineType=cv2.LINE_AA)
+                
+        cv2.imwrite("output/result_image.jpg", self.input_image)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--image', type=str, default="input\image.jpg")
+    parser.add_argument('--image', type=str, default="input/image.jpg")
     parser.add_argument('--threshold', type=str, default=25)
     parser.add_argument('--update')
 
     opt = parser.parse_args()
+
+    cls = FaceIdentification()
